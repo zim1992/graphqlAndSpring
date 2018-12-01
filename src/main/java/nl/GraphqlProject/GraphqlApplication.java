4457aab07f9bdc6ddfb6f46aa30c41b6c3b3f9e5
Zim1992 <Zim1992@github.com>
2018-12-01 11:17:08 +0100

    adding a sql server to the application. and application properties.

--- src/main/java/nl/GraphqlProject/GraphqlApplication.java
+++ src/main/java/nl/GraphqlProject/GraphqlApplication.java
@@ -1,25 +1,28 @@
 package nl.GraphqlProject;
 
+import nl.GraphqlProject.entities.Pet;
+import nl.GraphqlProject.enums.Animal;
+import nl.GraphqlProject.repositories.PetRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
-public class GraphqlApplication implements CommandLineRunner {
+public class GraphqlApplication{
     private static final Logger log = LoggerFactory.getLogger(GraphqlApplication.class);
     public static void main(String[] args){
         SpringApplication.run(GraphqlApplication.class, args);
     }
 
-    @Autowired
-
-
-    @Override
-    public void run(String... args) throws Exception {
-        log
+    @Bean
+    CommandLineRunner runner(PetRepository repository){
+        return args -> {
+            repository.save(Pet.Create("Ben", Animal.Dog, 15 ));
+        };
     }
+
 }
--- src/main/java/nl/GraphqlProject/entities/Pet.java
+++ src/main/java/nl/GraphqlProject/entities/Pet.java
@@ -32,4 +32,45 @@ public class Pet {
         return new Pet(name, type, age);
     }
 
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Animal getType() {
+        return type;
+    }
+
+    public void setType(Animal type) {
+        this.type = type;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Pet{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", type=" + type +
+                ", age=" + age +
+                '}';
+    }
 }
